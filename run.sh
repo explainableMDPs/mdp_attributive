c95aaa2aa123f3e8960e9a460d7de8426bff7e4b
Paul Kobi <paulkob@ifi.uio.no>
2025-07-16 22:32:51 +0000

    run file

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python3 benchmarks.py --experiments greps bpic12 bpic17-before bpic17-after bpic17-both spotify --steps 10 --iterations 10 --cores 60 --timeout 60  --model_iterations 10 --rebuild_models --all_spotify
+python3 benchmarks.py --experiments greps bpic12 bpic17-before bpic17-after bpic17-both --model_iterations 10 --iterations 5 --cores 50 --rebuild_models
+# spotify3000 spotify5000 spotify7000 spotify1000 spotify3000 spotify5000 spotify9000
